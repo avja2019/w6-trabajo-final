a8e1eb362954b14ef536da80819827b16cd2980f
Jhon Antony <jhonatoni9.81@gmail.com>
2024-08-26 17:52:07 -0500

    test de category

--- src/tests/category.test.js
+++ src/tests/category.test.js
@@ -1,4 +1,7 @@
+const request = require("supertest")
+const app = require('../app')
 
+let categoryId
 
 const  BASE_URL = '/api/v1/categories'
 const  BASE_URL_LOGIN = '/api/v1/users/login'
@@ -21,13 +24,30 @@ const category = {
 
 test("POST -> BASE_URL, should return statusCode 201, res.body.name === category.name", async () => {
     const res = await request(app)
-      .post(BASE_URL)
-      .send(category)
-      .set(`Authorization`, `Bearer ${TOKEN}`)
+        .post(BASE_URL)
+        .send(category)
+        .set(`Authorization`, `Bearer ${TOKEN}`)
   
     categoryId = res.body.id
   
     expect(res.statusCode).toBe(201)
     expect(res.body).toBeDefined()
     expect(res.body.name).toBe(category.name)
+})
+
+test("GET --> BASE_URL, should return statusCode 200, and res.body.length === 1", async () => {
+    const res = await request(app)
+        .get(BASE_URL)
+    // console.log(res.body)
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
   })
+
+test("DELETE -> BASE_URL/categoryId, should return statusCode 204, and res.body.name === category.name", async () => {
+    const res = await request(app)
+        .delete(`${BASE_URL}/${categoryId}`)
+        //un send se utiliza para enviar un body por eso en el delite no se utiliza
+        .set('Authorization', `Bearer ${TOKEN}`)
+    expect(res.statusCode).toBe(204)
+})
